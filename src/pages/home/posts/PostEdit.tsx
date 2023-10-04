cec42e5ff9a6f29f56d87050ae2ed86b2642ad3b
1eeyerin <dev.yerin@gmail.com>
2023-10-04 16:25:41 +0900

    feat: set router, outlet

---
+++ src/pages/home/posts/PostEdit.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PostEdit = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default PostEdit;
---
+++ src/pages/home/posts/index.ts
@@ -0,0 +1,4 @@
+export {default as PostDetail} from './PostDetail';
+export {default as PostEdit} from './PostEdit';
+export {default as PostList} from './PostList';
+export {default as PostNew} from './PostNew';
